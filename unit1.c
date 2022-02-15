94fde2b79767af701ef3cf0c141d981e5c807d70
Baedonguri <71864367+baedonguri@users.noreply.github.com>
2022-02-15 18:12:15 +0900

    first commit

---
+++ unit1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    
+    int x,y,sum;
+    x = 1;
+    y = 2;
+    sum = x+y;
+
+    printf("The answer is %i\n", sum);
+    return 0;
+}  
---
+++ unit2.9_make_function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void say_hello(void); // prototyping, function declaration
+
+
+int main(){
+    say_hello();
+}
+
+
+void say_hello(void){ // function definition
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+}
